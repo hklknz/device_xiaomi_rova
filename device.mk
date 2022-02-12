6f30ee2ccf60a95e61fb47c65c605b8223c87684
blazerpaul <blazerpaul15@gmail.com>
2022-02-12 21:26:31 +0800

    rova: Add prebuilt Graphene Camera
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -105,6 +105,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6 \
     libstdc++.vendor
 
+PRODUCT_PACKAGES += \
+    GrapheneCamera
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
